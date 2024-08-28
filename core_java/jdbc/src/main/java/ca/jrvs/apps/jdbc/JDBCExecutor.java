5f51d0911d98a6198a02964e34ff0e69ce9c9c75
hg591038 <kaissar.cara@courrier.uqam.ca>
2024-08-27 23:30:53 -0400

    LinkedInCourse complete DAO implementation

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,7 +1,37 @@
 package ca.jrvs.apps.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
     public static void main(String[] args) {
-        System.out.println("Learning JDBC");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost","hplussport","postgres","newpassword");
+        try{
+            Connection connection = dcm.getConnection();
+            CustomerDAO custDao = new CustomerDAO(connection);
+            Customer customer = new Customer();
+            customer.setFirstName("John");
+            customer.setLastName("Adams");
+            customer.setEmail("jadams.wh.gov");
+            customer.setAddress("1234 Main St");
+            customer.setCity("Arlington");
+            customer.setState("VA");
+            customer.setPhone("(555) 555-9845");
+            customer.setZipCode("01234");
+
+            Customer dbCustomer = custDao.create(customer);
+            System.out.println(dbCustomer);
+            dbCustomer = custDao.findById(dbCustomer.getId());
+            System.out.println(dbCustomer);
+            dbCustomer.setEmail("john.adams@wh.gov");
+            dbCustomer = custDao.update(dbCustomer);
+            System.out.println(dbCustomer);
+            custDao.delete(dbCustomer.getId());
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
